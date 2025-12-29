23702c449e72666cd6c6256d026e28395adfc3fe
sesetty bhargavi <sesettybhargavi@gmail.com>
2025-12-29 19:06:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 //document.addEventListener("DOMContentLoaded", function(){
-let element = document.getElementByID("level");
+let element = document.getElementById("level");
 let level = 0;
 
 while(element){
